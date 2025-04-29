fe33162033df2e9c2b91ab627f8e88b7ad8f5401
samuraypro7767 <samuraypro7767@gmail.com>
2025-04-29 06:22:14 -0500

    bitacora repository creado y estructurado

---
+++ src/main/java/com/ludicamente/Ludicamente/repository/BitacoraRepository.java
@@ -0,0 +1,7 @@
+package com.ludicamente.Ludicamente.repository;
+
+import com.ludicamente.Ludicamente.model.Bitacora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BitacoraRepository extends JpaRepository<Bitacora, Integer> {
+}
