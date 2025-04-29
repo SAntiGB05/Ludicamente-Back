01ddce4fd73c71130eacd56900ab42576bc457c2
luiscar2050 <luis3206162549@gmail.com>
2025-04-29 09:15:29 -0500

    terminado los 4 servicios de categoria

--- src/main/java/com/ludicamente/Ludicamente/repository/CategoriaRepository.java
+++ src/main/java/com/ludicamente/Ludicamente/repository/CategoriaRepository.java
@@ -1,4 +1,7 @@
 package com.ludicamente.Ludicamente.repository;
 
-public interface CategoriaRepository {
+import com.ludicamente.Ludicamente.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
 }
