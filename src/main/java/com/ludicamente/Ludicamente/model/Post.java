eb77774852c37e606402a3ff5f2ef39f518c382f
luiscar2050 <luis3206162549@gmail.com>
2025-04-30 09:41:39 -0500

    servicios terminados

--- src/main/java/com/ludicamente/Ludicamente/model/Post.java
+++ src/main/java/com/ludicamente/Ludicamente/model/Post.java
@@ -53,4 +53,92 @@ public class Post {
     public Post() {
         this.fechaPublicacion = LocalDateTime.now();
     }
+
+    public Integer getIdPost() {
+        return idPost;
+    }
+
+    public void setIdPost(Integer idPost) {
+        this.idPost = idPost;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public LocalDateTime getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalDateTime fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public LocalDateTime getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setFechaActualizacion(LocalDateTime fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+    public EstadoPost getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoPost estado) {
+        this.estado = estado;
+    }
+
+    public String getImagenDestacada() {
+        return imagenDestacada;
+    }
+
+    public void setImagenDestacada(String imagenDestacada) {
+        this.imagenDestacada = imagenDestacada;
+    }
+
+    public String getResumen() {
+        return resumen;
+    }
+
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+    public String getEtiquetas() {
+        return etiquetas;
+    }
+
+    public void setEtiquetas(String etiquetas) {
+        this.etiquetas = etiquetas;
+    }
+
+    public Integer getVisitas() {
+        return visitas;
+    }
+
+    public void setVisitas(Integer visitas) {
+        this.visitas = visitas;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
 }
--- src/main/java/com/ludicamente/Ludicamente/repository/PostRepository.java
+++ src/main/java/com/ludicamente/Ludicamente/repository/PostRepository.java
@@ -1,4 +1,20 @@
 package com.ludicamente.Ludicamente.repository;
 
-public interface PostRepository {
+import com.ludicamente.Ludicamente.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    // Buscar posts por estado
+    List<Post> findByEstado(Post.EstadoPost estado);
+
+    List<Post> findByTituloContainingIgnoreCaseOrContenidoContainingIgnoreCaseOrEtiquetasContainingIgnoreCase(
+            String titulo, String contenido, String etiquetas);
+
+    // Buscar posts por empleado
+    List<Post> findByEmpleado_IdEmpleado(Integer idEmpleado);
 }
