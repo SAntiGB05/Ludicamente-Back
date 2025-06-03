6e9560b1abde07582aedba63a235c3b095f2f6c2
samuraypro7767 <samuraypro7767@gmail.com>
2025-06-03 11:42:20 -0500

    cambios

--- src/main/java/com/ludicamente/Ludicamente/config/SecurityConfig.java
+++ src/main/java/com/ludicamente/Ludicamente/config/SecurityConfig.java
@@ -17,12 +17,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
+@CrossOrigin
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
