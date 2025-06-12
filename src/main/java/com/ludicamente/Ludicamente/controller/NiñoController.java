12c6bb88564ea4552314cfb5a1c6753a67188ca0
samuraypro7767 <sebaspro7767@gmail.com>
2025-06-12 13:57:57 -0500

    modificacion de foto

--- src/main/java/com/ludicamente/Ludicamente/controller/NiñoController.java
+++ src/main/java/com/ludicamente/Ludicamente/controller/NiñoController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collection;
 import java.util.List;
@@ -96,6 +97,16 @@ public class NiñoController {
         Optional<NiñoDto> niño = niñoService.actualizarNiño(id, niñoActualizado);
         return niño.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+
+    @PutMapping("/{id}/foto")
+    public ResponseEntity<Void> actualizarFotoNiño(
+            @PathVariable Integer id,
+            @RequestParam("foto") MultipartFile foto) {
+        niñoService.actualizarFoto(id, foto);
+        return ResponseEntity.ok().build();
+    }
+
 
     // Eliminar un niño
     @Operation(summary = "Eliminar un niño")
